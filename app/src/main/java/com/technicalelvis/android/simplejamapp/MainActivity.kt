bb2ef5b433fa9512a374eb98bf03a2561e37ae6d
Telvis Calhoun <telvis07@gmail.com>
2020-12-12 11:05:25 -0500

    start

---
+++ app/src/main/java/com/technicalelvis/android/simplejamapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.technicalelvis.android.simplejamapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
